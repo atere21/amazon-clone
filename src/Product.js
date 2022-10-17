b2d89b569f991300ad1cc871608201574d8ef72b
atere21 <tosinateret@gmail.com>
2022-10-17 08:02:53 -0500

    amazon-clone

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className='product'>
         <div className='product__info'>
-            <h3>Get fit at home</h3>
+            <h3>{title}</h3>
             <p className='product__price'>
             <small>$</small>
-            <strong>29.99</strong>
+            <strong>{price}</strong>
             </p>
             <div className='product__rating'>
-              <p>⭐</p>
+              {Array(rating).fill()
+              .map((_, i) => (
+                 <p>⭐</p>
+              ))}
+              
             </div>
         </div>
-        <img src='https://images-na.ssl-images-amazon.com/images/G/01/events/GFAH/GWDesktop_SingleImageCard_fitathome_1x._SY304_CB434924743_.jpg' 
-         alt='product_img'/>
+        <img src={image} alt='' />
         <button>Add to Basket</button>
         
     </div>
