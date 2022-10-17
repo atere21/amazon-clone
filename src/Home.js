46e0920004c5d048b96071d9ff36d6b561f398e1
atere21 <tosinateret@gmail.com>
2022-10-17 08:03:39 -0500

    amazon-clone

--- src/Home.js
+++ src/Home.js
@@ -1,47 +1,45 @@
-import React from 'react'
-import './Home.css'
-import Product from './Product'
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
-    <div className='home'>
-       <div className='home__container'>
-          <img className='home__image'
-           src='https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3000_.jpg' alt='home-img'></img>
-       
-        <div className='home__row'>
-            <Product/>
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3000_.jpg"
+          alt="home-img"
+        ></img>
 
-            {/*product*/}
-            {/*product*/}
-            {/*product*/}
+        <div className="home__row">
+          <Product title='Get fit at home' 
+          price={29.99} 
+          image='https://images-na.ssl-images-amazon.com/
+          images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Laptops_379x304_1X_en_US._SY304_CB418608471_.jpg'
+          rating={5}/>
+          <Product />
+          <Product />
+          <Product />
         </div>
-        <div className='home__row'>
-            {/*product*/}
-            {/*product*/}
-            {/*product*/}
-            {/*product*/}
-        
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
         </div>
-        <div className='home__row'>
-            {/*product*/}
+        <div className="home__row"><Product /></div>
+        <div className="home__row"><Product /></div>
+        <div className="home__row">
+         <Product />
+          <Product />
+          <Product />
+          <Product />
         </div>
-        <div className='home__row'>
-            {/*product*/}
-        </div>
-        <div className='home__row'>
-            {/*product*/}
-            {/*product*/}
-            {/*product*/}
-            {/*product*/}
-        
-        </div>
-        <div className='home__row'>
-            {/*product*/}
-        </div>
-        </div>
+        <div className="home__row"><Product /></div>
+      </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
