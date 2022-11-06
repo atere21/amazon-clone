2b298a2bd6917a36c10fbb457a5087a571c720cc
atere21 <tosinateret@gmail.com>
2022-11-05 23:58:23 -0600

    react-router-implemented

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import React from 'react';
 import './App.css';
 import Header from './Header'
 import Home from './Home'
-import { BrowserRouter as Router, Switch, Route }
-from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } 
+// from "react-router-dom";
+import Checkout from "./Checkout"
 
 function App() {
   return (
     <Router>
     <div className="app">
-      <switch>
-        <Route path='/'>
-      <Header />
-      <Home />
-      </Route>
-      </switch>
+       
+      
+      
 
-    </div>
-    </Router>
-  );
+        {/* 👇️ Wrap your Route components in a Routes component */}
+        <Routes>
+          <Route path="/Checkout" element={<><Header /> <Checkout/></>} />
+          <Route exact path="/" element={<><Header/><Home/></>}/>
+          {/* <Route path="/"  element={<Home />} /> */}
+        </Routes>
+        </div>
+       </Router>
+    
+    
+  )
 }
 
 export default App;
+
